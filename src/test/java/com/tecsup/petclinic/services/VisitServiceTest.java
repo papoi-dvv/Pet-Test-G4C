b79e8ce1bee6adff0bc0ea5852ea628168c998ef
GARCIA <cristian.garcia.c@tecsup.edu.pe>
2025-10-27 23:57:30 +0100

    Update VisitServiceTest.java

--- src/test/java/com/tecsup/petclinic/services/VisitServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VisitServiceTest.java
@@ -4,29 +4,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.time.LocalDate;
-
 import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @Slf4j
 @SpringBootTest
 public class VisitServiceTest {
-
     @Autowired
     private VisitService visitService;
-
     @Test
     public void testDeleteVisit() {
-
         String DESCRIPTION = "";
         double COST = 50.00;
         Integer PET_ID = 1;
         Integer VET_ID = 1;
         LocalDate VISIT_DATE = LocalDate.now();
-
         VisitDTO visitDTO = VisitDTO.builder()
                 .description(DESCRIPTION)
                 .cost(COST)
@@ -34,7 +27,6 @@ public class VisitServiceTest {
                 .vetId(VET_ID)
                 .visitDate(VISIT_DATE)
                 .build();
-
         VisitDTO newVisitDTO = this.visitService.create(visitDTO);
         log.info(" " + newVisitDTO);
 
@@ -43,7 +35,6 @@ public class VisitServiceTest {
         } catch (VisitNotFoundException e) {
             fail(e.getMessage());
         }
-
         try {
             this.visitService.findById(newVisitDTO.getId());
             assertTrue(false, "Visit n");
